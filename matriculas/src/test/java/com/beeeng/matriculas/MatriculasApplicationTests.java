8710a2f5ffb615b2e4594df5d0f0ebc891407fe1
Adriano <a19ferreira@gmail.com>
2019-01-10 16:53:20 +0000

    Add files via upload

---
+++ matriculas/src/test/java/com/beeeng/matriculas/MatriculasApplicationTests.java
@@ -0,0 +1,17 @@
+package com.beeeng.matriculas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatriculasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
